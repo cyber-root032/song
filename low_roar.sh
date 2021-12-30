eaeb1478c0c8d70faba0a5b395769e4779344ce4
Arthur <68313618+cyber-root032@users.noreply.github.com>
2021-12-30 13:14:59 -0300

    Add files via upload

---
+++ low_roar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Just a song
+echo "Welcome $USER  let's sing "
+echo "**Low Roar - Anything you need**"
+sleep 6
+echo "Come in, come in, come in" 
+sleep 6
+echo "Take anything you want from me"
+sleep 6
+echo "Come in, come in, come in"
+sleep 6
+echo "Take anything you need"
+sleep 6
+echo "Anything you need"
+sleep 10
+echo "So come in, come in, come in"
+sleep 6 
+echo "Take anything you want from me"
+sleep 6
+echo "Come in, Come in, Come in"
+sleep  6
+echo "Take anything you need"
+sleep 6
+echo "Anything you need"
+echo "https://www.youtube.com/watch?v=OzXAIB7LWDM"
